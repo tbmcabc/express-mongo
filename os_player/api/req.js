5b8cee7a40a35456f4171c9ae1c198af53cb9f6c
yxy <645455385@qq.com>
2020-03-11 18:53:47 +0800

    xxx

--- os_player/api/req.js
+++ os_player/api/req.js
@@ -2,12 +2,14 @@ const express = require('express')
 var urlencode = require('urlencode')
 let router = express.Router();
 
+const aesutil = require('../../utils/aesutil')
+
 
 router.get('/c2s_request', function (req, res, next) {
     res.send("ok")
 })
 
-router.get('/c2s_redisset', function (req, res, next) {    
+router.get('/c2s_redisset', function (req, res, next) {
     app.redis_cli.hset("hash1", "vv", "1", function (err, result) {
         if (err) {
             res.json({
@@ -24,7 +26,7 @@ router.get('/c2s_redisset', function (req, res, next) {
     })
 })
 
-router.get('/c2s_redisget', function (req, res, next) {    
+router.get('/c2s_redisget', function (req, res, next) {
     app.redis_cli.hget("hash1", "vv", function (err, result) {
         if (err) {
             res.json({
@@ -38,25 +40,35 @@ router.get('/c2s_redisget', function (req, res, next) {
                 result: JSON.stringify(result)
             })
         }
-    })    
+    })
 })
 
-router.get('/requestapi', function (req, res, next) {   
-    console.log(req) 
-    let newreq = urlencode.decode(req)
-    console.log(newreq)
+router.get('/requestapi', function (req, res, next) {
     let msg_signature = req.query.msg_signature;
-    let echostr = req.query.echostr;
+    let echostr = urlencode.decode(req.query.echostr);
     let timestamp = req.query.timestamp;
     let nonce = req.query.nonce
-    console.log(msg_signature)
-    console.log(echostr)
-    console.log(timestamp)
-    console.log(nonce)
+    let str = verifyUrl(msg_signature, timestamp, nonce, echostr)
 
 
-    res.send()
+    res.send(str)
 })
+
+
+function verifyUrl(msg_signature, timestamp, nonce, echostr) {
+    let str = new Buffer.from('aszrgmMaa9UVZdluGbgJPqXqNnFO6xabTKIJrXRCQPr'+ "=", "base64")
+    let AESKey = str.toString()
+    console.log(AESKey.length)
+    let newEchostr = "";
+    console.log("msg_signature \n" + msg_signature)
+    console.log("echostr \n" + echostr)
+    console.log("timestamp \n" + timestamp)
+    console.log("nonce \n" + nonce)    
+    let randommsg = aesutil.decryption(echostr,str)
+    console.log(randommsg)
+
+    return newEchostr
+}
 
 
 
