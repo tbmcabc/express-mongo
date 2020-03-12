d772255447da174b0b2670b1877169b1b6d13262
yxy <645455385@qq.com>
2020-03-12 08:31:14 +0800

    xxx

--- os_player/api/req.js
+++ os_player/api/req.js
@@ -5,8 +5,9 @@ let router = express.Router();
 let recieveid = "ww0d1c5329d824145e"
 let encodingAesKey = "BkkFobAFthA5mU8gfRnMvMBRUMuLRvSybQMNi7fo7IO";
 let token = "RcCeXOhFuQ76AC9HlyWXiTBioampfa";
-
 const aesutil = require('../../utils/aesutil')
+var CryptoJS = require('crypto-js')
+var Base64 = require('../../utils/base64')
 
 
 router.get('/c2s_request', function (req, res, next) {
@@ -49,7 +50,11 @@ router.get('/c2s_redisget', function (req, res, next) {
 
 router.get('/requestapi', function (req, res, next) {
     let msg_signature = req.query.msg_signature;
-    let echostr = urlencode.decode(req.query.echostr);
+    let echostr1 = urlencode.decode(req.query.echostr);
+    let echostr = req.query.echostr;
+    console.log(echostr)
+    console.log(echostr1)
+
     let timestamp = req.query.timestamp;
     let nonce = req.query.nonce
     let str = verifyUrl(msg_signature, timestamp, nonce, echostr)
@@ -60,26 +65,58 @@ router.get('/requestapi', function (req, res, next) {
 
 
 function verifyUrl(msg_signature, timestamp, nonce, echostr) {
-    console.log(encodingAesKey.length)
+    // console.log(encodingAesKey.length)
     if (encodingAesKey.length != 43) {
         return "false"
     }
     let sha1 = crypto.createHash('sha1')
+
     let params = [token,timestamp,nonce,echostr]
-    console.log(params)
+    // console.log(params)
     params.sort()
-    console.log(params)
+    // console.log(params)
     let d1 = sha1.update(params[0]+params[1]+params[2]+params[3]).digest("hex")
-    console.log("加密的结果：" + d1)
+    // // let key = Base64.decode(encodingAesKey +"=")
+    // let base64 = new Base64()
+
+    // // 密钥 32 位
+    let aeskey  = Buffer.from(encodingAesKey + '=',"base64")
+    let s =  aeskey
+    // console.log(s)
+    // console.log(s.length)
+    // // // 初始向量 initial vector 16 位
+    let iv  = s.slice(0,16)
+
+    // console.log(echostr)
+
+    
+
+    let encryptedText =Buffer.from(echostr,'base64');
+
+    // let decipher =crypto.createDecipheriv('aes-256-cbc',aeskey, iv);
+
+    // let decrypted =decipher.update(encryptedText);
+
+    // decrypted =Buffer.concat([decrypted,decipher.final()]);
+
+     
+    //解密
+    var decrypted = CryptoJS.AES.decrypt(encryptedText, aeskey, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+     
+    console.log(decrypted)
+    // // 转换为 utf8 字符串
+    // decrypted = aesutil.decryption(echostr,key1,'')
+    // console.log("加密的结果：" + d1)
     if(d1 != msg_signature){
         return "false"
     }else{
+        //对密文进行base64解码
         return "true"
     }
-
-    console.log(msg_signature)
-
-
     return "newEchostr"
 }
 
--- utils/aesutil.js
+++ utils/aesutil.js
@@ -5,7 +5,7 @@ var aesutil = module.exports = {};
 /**
  * aes加密
  * @param data 待加密内容
- * @param key 必须为32位私钥
+ * @param key 必须为16位私钥
  * @returns {string}
  */
 aesutil.encryption = function (data, key, iv) {
@@ -13,7 +13,7 @@ aesutil.encryption = function (data, key, iv) {
     var clearEncoding = 'utf8';
     var cipherEncoding = 'base64';
     var cipherChunks = [];
-    var cipher = crypto.createCipheriv('aes-256-ecb', key, iv);
+    var cipher = crypto.createCipheriv('aes-128-ecb', key, iv);
     cipher.setAutoPadding(true);
     cipherChunks.push(cipher.update(data, clearEncoding, cipherEncoding));
     cipherChunks.push(cipher.final(cipherEncoding));
@@ -23,20 +23,17 @@ aesutil.encryption = function (data, key, iv) {
 /**
  * aes解密
  * @param data 待解密内容
- * @param key 必须为32位私钥
+ * @param key 必须为16位私钥
  * @returns {string}
  */
-aesutil.decryption = function (data, key, iv) {
-    if (!data) {
+aesutil.decryption = function (crypted, key, iv) {
+    if (!crypted) {
         return "";
     }
-    iv = iv || "";
-    var clearEncoding = 'utf8';
-    var cipherEncoding = 'base64';
-    var cipherChunks = [];
-    var decipher = crypto.createDecipheriv('aes-256-ecb', key, iv);
-    decipher.setAutoPadding(true);
-    cipherChunks.push(decipher.update(data, cipherEncoding, clearEncoding));
-    cipherChunks.push(decipher.final(clearEncoding));
-    return cipherChunks.join('');
+    // let padding = AES_conf.padding;
+    crypted = new Buffer(crypted, 'base64').toString('binary');
+    var decipher = crypto.createDecipher('aes-128-cbc', key,'');
+    var decoded = decipher.update(crypted, 'binary', 'utf8');
+    decoded += decipher.final('utf8');
+    return decoded;
 }
